aaaee990df92d6e45993ca42cf03a0709464c2c8
Mis-291205 <cleopatrayes17@gmail.com>
2025-09-15 17:14:43 +0700

    Route

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
 import { Grid, GridItem } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -12,8 +17,19 @@ function App() {
         <NavBar />
       </GridItem>
 
-      <GridItem area="main" width="100%" maxWidth="100vw" overflowX="hidden">
-        Main Content
+      <GridItem
+        area="main"
+        width="100%"
+        maxWidth="100vw"
+        overflowX="hidden"
+        p={5}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
       </GridItem>
     </Grid>
   );
