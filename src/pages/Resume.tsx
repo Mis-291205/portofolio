e4325c4113353106d4b84cb2b376a265df3187e9
Mis-291205 <cleopatrayes17@gmail.com>
2025-09-19 23:36:01 +0700

    fix resume

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -36,7 +36,7 @@ const Resume = () => {
         transition={{ duration: 0.8, ease: "easeOut", delay: 0.2 }}
       >
         <iframe
-          src="/Michael Ivan - CV.pdf"
+          src="/Michael Ivan - CV - Resume.pdf"
           width="100%"
           height="100%"
           style={{ border: "none" }}
@@ -45,7 +45,7 @@ const Resume = () => {
       </MotionBox>
 
       <Link
-        href="/Michael Ivan - CV.pdf"
+        href="/Michael Ivan - CV - Resume.pdf"
         download
         _hover={{ textDecoration: "none" }}
       >
